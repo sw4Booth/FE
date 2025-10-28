eb5e89acf5a5dbabc366a39d2abb2bf442468364
Suhan Ha <10433434+chemistryx@users.noreply.github.com>
2025-10-29 00:13:10 +0900

    feat: React Router 도입

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
+import { BrowserRouter, Route, Routes } from "react-router";
+import Home from "./pages/Home";
+import MainLayout from "./layouts/MainLayout";
+
 function App() {
     return (
-        <>
-            <h1 className="text-3xl font-bold underline">sw4booth-fe</h1>
-        </>
+        <BrowserRouter>
+            <Routes>
+                <Route element={<MainLayout />}>
+                    <Route index element={<Home />} />
+                </Route>
+            </Routes>
+        </BrowserRouter>
     );
 }
 
