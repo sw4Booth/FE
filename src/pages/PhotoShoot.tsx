3436f87b05cac4e552eea7c134b53f21033ed324
zldn109 <ss771009@naver.com>
2025-10-30 16:58:06 +0900

    feat: 브라우저저API 이용해서 웹캠 접근 및 촬영 기능 구현

--- src/pages/PhotoShoot.tsx
+++ src/pages/PhotoShoot.tsx
@@ -1,13 +1,79 @@
 import Heading from "../components/Heading";
+import { useEffect, useRef, useState } from "react";
 
 export default function PhotoShoot() {
+  const videoRef = useRef<HTMLVideoElement | null>(null);
+  const [currentCount, setCurrentCount] = useState(0);
+  const [photos, setPhotos] = useState<string[]>([]);
+
+  const TOTAL_SHOTS = 8;
+  const INTERVAL = 7000;
+
+  useEffect(() => {
+    async function setupCamera() {
+      try {
+        const stream = await navigator.mediaDevices.getUserMedia({
+          video: true,
+        });
+        if (videoRef.current) {
+          videoRef.current.srcObject = stream;
+        }
+      } catch (err) {
+        console.error(err);
+      }
+    }
+
+    async function startShooting() {
+      for (let i = 0; i < TOTAL_SHOTS; i++) {
+        await new Promise((res) => setTimeout(res, INTERVAL));
+        takePhoto();
+        setCurrentCount(i + 1);
+      }
+    }
+
+    // setupCamera();
+    // startShooting();
+
+    return () => {
+      //다음페이지로 넘어가면 종료
+      if (videoRef.current && videoRef.current.srcObject) {
+        const tracks = (videoRef.current.srcObject as MediaStream).getTracks();
+        tracks.forEach((track) => track.stop());
+      }
+    };
+  }, []);
+
+  const takePhoto = () => {
+    if (!videoRef.current) return;
+    const video = videoRef.current;
+    const canvas = document.createElement("canvas");
+    canvas.width = video.videoWidth;
+    canvas.height = video.videoHeight;
+
+    const ctx = canvas.getContext("2d")!;
+    ctx.translate(canvas.width, 0);
+    ctx.scale(-1, 1);
+    ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+
+    const dataURL = canvas.toDataURL("image/png");
+    setPhotos((prev) => [...prev, dataURL]);
+  };
+
   return (
-    <div>
-          <Heading>사진 촬영</Heading>
-          <div>
-              <div></div>
-              <div></div>
-          </div>
+    <div className="flex flex-col w-full h-full items-center">
+      <Heading>사진 촬영</Heading>
+      <div>
+        <div></div>
+        <div></div>
+      </div>
+      <div className="w-[80%] aspect-[7/5] bg-gray-200">
+        <video
+          ref={videoRef}
+          autoPlay
+          playsInline
+          className="w-full h-full object-cover -scale-x-100"
+        />
+      </div>
     </div>
   );
 }
