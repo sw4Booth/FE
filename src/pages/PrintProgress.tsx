427d08cb98e4ece56155ed00b9a169a9424effc0
Suhan Ha <10433434+chemistryx@users.noreply.github.com>
2025-11-01 20:46:05 +0900

    refactor: flex grow 적용

--- src/pages/PrintProgress.tsx
+++ src/pages/PrintProgress.tsx
@@ -2,12 +2,12 @@ import Heading from "../components/Heading";
 
 export default function PrintProgress() {
     return (
-        <div className="flex flex-col w-full h-full items-center">
+        <div className="flex flex-col w-full h-full items-center gap-6">
             <Heading>사진 출력이 진행중입니다!</Heading>
-            <div className="flex flex-col items-center justify-center gap-12 w-[80%] h-[50%] my-24 p-12 bg-primary-100 rounded-lg">
+            <div className="flex flex-col grow items-center justify-center gap-12 w-[70%] p-12 bg-primary-100 rounded-lg">
                 <span>미리보기 영역</span>
             </div>
-            <span className="text-2xl font-semibold text-primary-600">잠시만 기다려주세요..!</span>
+            <span className="text-2xl font-medium text-primary-600">잠시만 기다려주세요...</span>
         </div>
     );
 }
