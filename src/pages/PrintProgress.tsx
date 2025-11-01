5f2142e539fb384fe5133b79b25ab40efca9fe12
Suhan Ha <10433434+chemistryx@users.noreply.github.com>
2025-11-01 21:05:30 +0900

    feat: 출력 로딩 페이지 기본 틀 구현 (#15)
    * feat: 출력 진행 페이지 초기 구현
    
    * refactor: flex grow 적용
    
    * feat: Route 추가

---
+++ src/pages/PrintProgress.tsx
@@ -0,0 +1,13 @@
+import Heading from "../components/Heading";
+
+export default function PrintProgress() {
+    return (
+        <div className="flex flex-col w-full h-full items-center gap-6">
+            <Heading>사진 출력이 진행중입니다!</Heading>
+            <div className="flex flex-col grow items-center justify-center gap-12 w-[70%] p-12 bg-primary-100 rounded-lg">
+                <span>미리보기 영역</span>
+            </div>
+            <span className="text-2xl font-medium text-primary-600">잠시만 기다려주세요...</span>
+        </div>
+    );
+}
