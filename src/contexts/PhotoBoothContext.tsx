85bc939796b27bfdc2b4336406ea75805aa24527
zldn109 <163500324+zldn109@users.noreply.github.com>
2025-11-01 19:31:36 +0900

    feat: Photobooth shoot page 구현 (#8)
    * feat: 라우트 연결
    
    * feat: 브라우저저API 이용해서 웹캠 접근 및 촬영 기능 구현
    
    * feat: 남은시간, 찍은장수 표시 기능 구현
    
    * update: 캡처 이미지 저장 방식을 base64 → File로 변경
    
    * refactor: eslint 설정 수정
    
    * chore: 프리티어 설정
    
    * refactor: 상수 위치, 이름 수정
    
    * refactor: file 변환 로직 수정
    
    * style: 남은 시간 띄우는 방법 변환
    
    * refactor: 초단위 미리초로 통합 및 takePhoto() 로직 수정

--- src/contexts/PhotoBoothContext.tsx
+++ src/contexts/PhotoBoothContext.tsx
@@ -2,21 +2,23 @@ import { createContext, type Dispatch, type SetStateAction } from "react";
 import type { FrameType } from "../types/FrameType";
 
 export interface PhotoBoothState {
-    frameType: FrameType | null;
-    capturedPhotos: File[];
-    selectedPhotos: File[];
-    backgroundFrame: string | null;
-    printCount: number;
-    shouldPublishToGuestbook: boolean;
+  frameType: FrameType | null;
+  capturedPhotos: File[];
+  selectedPhotos: File[];
+  backgroundFrame: string | null;
+  printCount: number;
+  shouldPublishToGuestbook: boolean;
 }
 
 export interface PhotoBoothContextType extends PhotoBoothState {
-    setFrameType: Dispatch<SetStateAction<FrameType | null>>;
-    setCapturedPhotos: Dispatch<SetStateAction<File[]>>;
-    setSelectedPhotos: Dispatch<SetStateAction<File[]>>;
-    setBackgroundFrame: Dispatch<SetStateAction<string | null>>;
-    setPrintCount: Dispatch<SetStateAction<number>>;
-    setPublishToGuestbook: Dispatch<SetStateAction<boolean>>;
+  setFrameType: Dispatch<SetStateAction<FrameType | null>>;
+  setCapturedPhotos: Dispatch<SetStateAction<File[]>>;
+  setSelectedPhotos: Dispatch<SetStateAction<File[]>>;
+  setBackgroundFrame: Dispatch<SetStateAction<string | null>>;
+  setPrintCount: Dispatch<SetStateAction<number>>;
+  setPublishToGuestbook: Dispatch<SetStateAction<boolean>>;
 }
 
-export const PhotoBoothContext = createContext<PhotoBoothContextType | undefined>(undefined);
+export const PhotoBoothContext = createContext<
+  PhotoBoothContextType | undefined
+>(undefined);
