a83e0af96071820b0b231aa76d1991eb098a6c64
zldn109 <ss771009@naver.com>
2025-11-01 22:26:53 +0900

    feat:사진찍는페이지에서 사진 셀렉 페이지로 이동 구현

--- src/pages/PhotoSelect.tsx
+++ src/pages/PhotoSelect.tsx
@@ -1,3 +1,36 @@
+import Heading from "../components/Heading";
+import PhotoFrame from "../components/PhotoFrame";
+import { usePhotoBooth } from "../hooks/usePhotoBooth";
+
 export default function PhotoSelect() {
-    return <div>포토셀렉</div>;
+    const { capturedPhotos } = usePhotoBooth();
+    console.log(capturedPhotos);
+    return (
+        <div className="flex flex-col items-center w-full gap-10">
+            <Heading>사진을 선택해주세요</Heading>
+            <div className="flex flex-row w-full">
+                <div className="w-[30%]">
+                    <PhotoFrame
+                        frameType="landscape"
+                        className="h-[70vh] mx-auto"
+                    />
+                </div>
+
+                <div className="w-[65%] p-10 bg-primary-600">
+                    {capturedPhotos.length > 0 && (
+                        <div className="grid grid-cols-4">
+                            {capturedPhotos.map((file, i) => (
+                                <img
+                                    key={i}
+                                    src={URL.createObjectURL(file)}
+                                    alt={`photo-${i}`}
+                                    className="aspect-[7/5] object-cover rounded-md border"
+                                />
+                            ))}
+                        </div>
+                    )}
+                </div>
+            </div>
+        </div>
+    );
 }
