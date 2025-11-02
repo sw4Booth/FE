0ee7f05d5f5ad3af7e8fe2d5f6e02d04d646df3a
zldn109 <ss771009@naver.com>
2025-11-02 22:29:32 +0900

    feat: 라우터 연결

--- src/App.tsx
+++ src/App.tsx
@@ -5,21 +5,26 @@ import FrameType from "./pages/FrameType";
 import { PhotoBoothProvider } from "./contexts/PhotoBoothProvider";
 import PhotoShoot from "./pages/PhotoShoot";
 import Print from "./pages/Print";
+import BackFrameSelect from "./pages/BackFrameSelect";
 function App() {
-  return (
-    <PhotoBoothProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route element={<MainLayout />}>
-            <Route index element={<Start />} />
-            <Route path="/frame-type" element={<FrameType />} />
-            <Route path="/photo-shoot" element={<PhotoShoot />} />
-            <Route path="/print" element={<Print />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </PhotoBoothProvider>
-  );
+    return (
+        <PhotoBoothProvider>
+            <BrowserRouter>
+                <Routes>
+                    <Route element={<MainLayout />}>
+                        <Route index element={<Start />} />
+                        <Route path="/frame-type" element={<FrameType />} />
+                        <Route path="/photo-shoot" element={<PhotoShoot />} />
+                        <Route
+                            path="/back-frame-select"
+                            element={<BackFrameSelect />}
+                        />
+                        <Route path="/print" element={<Print />} />
+                    </Route>
+                </Routes>
+            </BrowserRouter>
+        </PhotoBoothProvider>
+    );
 }
 
 export default App;
---
+++ src/pages/BackFrameSelect.tsx
@@ -0,0 +1,3 @@
+export default function BackFrameSelect() {
+    return <div>배경프레임선택</div>;
+}
