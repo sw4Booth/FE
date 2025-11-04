09857781456ab5aa60681d18b6464e20c8b928e4
Suhan Ha <10433434+chemistryx@users.noreply.github.com>
2025-11-05 00:42:54 +0900

    refactor: route path 상수화

---
+++ src/constants/routes.tsx
@@ -0,0 +1,6 @@
+export const FRAME_TYPE_SELECT = "/frame-type-select";
+export const PHOTO_SHOOT = "/photo-shoot";
+export const PHOTO_SELECT = "/photo-select";
+export const PRINT = "/print";
+export const PRINT_PROGRESS = "/print-progress";
+export const SHARE = "/share";
--- src/pages/Start.tsx
+++ src/pages/Start.tsx
@@ -1,6 +1,7 @@
 import { useNavigate } from "react-router";
 import Heading from "../components/Heading";
 import Button from "../components/Button";
+import { FRAME_TYPE_SELECT } from "../constants/routes";
 
 export default function Start() {
     const navigate = useNavigate();
@@ -8,7 +9,7 @@ export default function Start() {
     return (
         <div className="flex flex-col w-full h-full py-12 items-center justify-around">
             <Heading>로고 영역</Heading>
-            <Button size="lg" onClick={() => navigate("/frame-type")}>시작하기</Button>
+            <Button size="lg" onClick={() => navigate(FRAME_TYPE_SELECT)}>시작하기</Button>
         </div>
     );
 }
