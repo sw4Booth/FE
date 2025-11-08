ce836b9fa0ee9ea07f92aa94c0e82e9486bfffd7
zldn109 <ss771009@naver.com>
2025-11-09 02:43:00 +0900

    refactor: 첨부 프레임 png로 변경해서 코드 최소화

--- src/frames/OceanGreedyFrame.tsx
+++ src/frames/OceanGreedyFrame.tsx
@@ -3,37 +3,10 @@ import type { FrameSkin } from "../types/Frame";
 export const OceanGreedyFrame: FrameSkin = {
     bgColor: "#0477BF",
     decorations: (
-        <>
-            <img
-                src={`/assets/frameImg/oceanGreedy/finish.png`}
-                alt="finish"
-                className="absolute top-0 left-0 w-full h-full object-contain pointer-events-none"
-            />
-            {/* {[
-                "bubble1",
-                "bubble2",
-                "bubble3",
-                "bubble4",
-                "bubble5",
-                "bubble6",
-                "bubble7",
-                "clam",
-                "fish1",
-                "fish2",
-                "oceanGreeny",
-                "octopus",
-                "seaweed",
-                "starfish",
-                "title",
-                "whale",
-            ].map((name, i) => (
-                <img
-                    key={i}
-                    src={`/assets/frameImg/oceanGreedy/${name}.png`}
-                    alt={name}
-                    className="absolute top-0 left-0 w-full h-full object-contain pointer-events-none"
-                />
-            ))} */}
-        </>
+        <img
+            src={`/assets/frameImg/oceanGreedy.png`}
+            alt="oceanGreedy"
+            className="absolute top-0 left-0 w-full h-full object-contain pointer-events-none"
+        />
     ),
 };
