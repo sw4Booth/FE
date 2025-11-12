d859323a782b9d2efc682c8d15349c49bdf26596
Suhan Ha <10433434+chemistryx@users.noreply.github.com>
2025-11-12 11:58:07 +0900

    refactor: 이미지 scale 수정

--- src/pages/FrameSkinSelect.tsx
+++ src/pages/FrameSkinSelect.tsx
@@ -44,7 +44,7 @@ export default function FrameSkinSelect() {
         if (!frameRef.current) return;
 
         try {
-            const canvas = await html2canvas(frameRef.current);
+            const canvas = await html2canvas(frameRef.current, { scale: 2 }); // TODO: 인쇄 상태에 따라 scale(DPI) 수정
 
             canvas.toBlob(async (blob) => {
                 if (!blob) return;
