87d1501851161923dd504de01b0f2f3c7e054df1
zldn109 <ss771009@naver.com>
2025-11-14 01:14:29 +0900

    feat: 2x6 클릭시 뿌옇게 + 체크 표시

--- src/pages/FrameTypeSelect.tsx
+++ src/pages/FrameTypeSelect.tsx
@@ -24,12 +24,22 @@ export default function FrameTypeSelect() {
             <Heading>가로 / 세로 프레임 선택</Heading>
             <div className="flex grow gap-32 items-center min-h-0">
                 <div
-                    className="flex flex-col items-center gap-6 p-8 h-full min-h-0 hover:bg-primary-100 data-[active=true]:bg-primary-100 rounded-lg cursor-pointer"
+                    className="relative flex flex-col items-center gap-6 p-8 h-full min-h-0 rounded-lg cursor-pointer data-[selected=true]:opacity-50"
                     onClick={() => handleFrameTypeClick("landscape")}
-                    data-active={frameType === "landscape"}
+                    data-selected={frameType === "landscape"}
                 >
                     <PhotoFrame frameType="landscape" />
                     <span className="text-lg font-semibold">가로 프레임</span>
+
+                    {frameType === "landscape" && (
+                        <div className="absolute inset-0 flex items-center justify-center z-10 rounded-md">
+                            <img
+                                src="/assets/check.svg"
+                                alt="check"
+                                className="w-25 h-25"
+                            />
+                        </div>
+                    )}
                 </div>
                 <div className="flex flex-col items-center gap-6 p-8 h-full min-h-0 rounded-lg opacity-50">
                     <PhotoFrame frameType="portrait" />
