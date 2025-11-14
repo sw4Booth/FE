69309c6517d08c1767101094161dedfb5b1a8825
zldn109 <ss771009@naver.com>
2025-11-14 23:45:12 +0900

    refactor: 버셀오류수정

--- src/pages/FrameTypeSelect.tsx
+++ src/pages/FrameTypeSelect.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import Button from "../components/Button";
 import Heading from "../components/Heading";
 import PhotoFrame from "../components/PhotoFrame";
