5927c9b39a5ecadf84272cefe64e4548b54bfeb2
zldn109 <ss771009@naver.com>
2025-11-17 16:20:16 +0900

    refactor: 재촬영시 상태 초기화

--- src/pages/Start.tsx
+++ src/pages/Start.tsx
@@ -3,17 +3,46 @@ import Heading from "../components/Heading";
 import Button from "../components/Button";
 import { FRAME_TYPE_SELECT } from "../constants/routes";
 import { SITE_BRANDING } from "../constants/constants";
+import { usePhotoBooth } from "../hooks/usePhotoBooth";
+import { useEffect } from "react";
+import { DefaultFrame } from "../frames/DefaultFrame";
 
 export default function Start() {
     const navigate = useNavigate();
+    const {
+        setFrameType,
+        setCapturedPhotos,
+        setSelectedPhotos,
+        setSelectedFrameSkin,
+        setPrintCount,
+        setPublishToGuestbook,
+        setUploadedPhotoId,
+        setQrImage,
+    } = usePhotoBooth();
+
+    useEffect(() => {
+        setFrameType(null);
+        setSelectedPhotos([]);
+        setCapturedPhotos([]);
+        setSelectedFrameSkin(DefaultFrame);
+        setPrintCount(2);
+        setPublishToGuestbook(true);
+        setUploadedPhotoId(-1);
+        setQrImage("");
+    }, []);
 
     return (
         <div className="flex flex-col w-full h-full py-12 items-center justify-center gap-12">
             <div className="flex flex-col items-center gap-6">
-                <img className="w-48 h-auto rounded-full" src="/assets/greedy_badge.jpg" />
+                <img
+                    className="w-48 h-auto rounded-full"
+                    src="/assets/greedy_badge.jpg"
+                />
                 <Heading>{SITE_BRANDING}</Heading>
             </div>
-            <Button size="lg" onClick={() => navigate(FRAME_TYPE_SELECT)}>시작하기</Button>
+            <Button size="lg" onClick={() => navigate(FRAME_TYPE_SELECT)}>
+                시작하기
+            </Button>
         </div>
     );
 }
