439ab2a4f364350beec09be09d23570c7a22bb34
Suhan Ha <10433434+chemistryx@users.noreply.github.com>
2025-11-17 16:55:09 +0900

    fix: 인쇄 영역 숨김 처리

--- src/pages/Print.tsx
+++ src/pages/Print.tsx
@@ -124,7 +124,7 @@ export default function Print() {
                 <Button size="lg" onClick={handlePrintClick}>출력하기</Button>
             </div>
             {mergedDataUrl && (
-                <div className="print:flex justify-center items-center w-full h-full">
+                <div className="hidden print:flex justify-center items-center w-full h-full">
                     <img src={mergedDataUrl} className="mb-[1mm] print:w-[102mm] print:h-[150mm] print:object-contain" />
                     <img src={mergedDataUrl} className="mb-[1mm] print:w-[102mm] print:h-[150mm] print:object-contain" />
                 </div>
